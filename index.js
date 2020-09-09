7e6f7d8c8a399d9b933ead5729011e4313c21788
qp092488 <AsiaEurope0131@gmail.com>
2020-09-09 10:52:55 +0900

    add clear command

--- index.js
+++ index.js
@@ -93,14 +93,14 @@ client.on('message', (message) => {
     }
   }
 
-  if(message.content.startsWith('!청소')) {
+  if(message.content.startsWith('<청소')) {
     if(checkPermission(message)) return
 
-    var clearLine = message.content.slice('!청소 '.length);
+    var clearLine = message.content.slice('<청소 '.length);
     var isNum = !isNaN(clearLine)
 
-    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
+    if(isNum && (clearLine <= 0 || 1000 < clearLine)) {
+      message.channel.send("1부터 1000까지의 숫자만 입력해주세요.")
       return;
     } else if(!isNum) { // c @나긋해 3
       if(message.content.split('<@').length == 2) {
