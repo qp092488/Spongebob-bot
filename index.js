22d1fe2ea70f1964b92b61cdc0a14c4486f0ed58
qp092488 <AsiaEurope0131@gmail.com>
2020-09-09 12:21:57 +0900

    add serverinfo

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.argv.length == 2 ? process.env.token : '';
+const token = process.argv.length == 2 ? process.env.token : "";
+const moment = require("moment");
+require("moment-duration-format");
 const welcomeChannelName = "💬채팅";
 const byeChannelName = "💬채팅";
 const welcomeChannelComment = "친구야 안녕 내 이름은 스펀지밥이야 ";
 const byeChannelComment = "잘가 친구";
 
-
 client.on('ready', () => {
   console.log('켰다.');
-  client.user.setPresence({ game: { name: '<help를 쳐보세요.' }, status: 'online' })
+  client.user.setPresence({ game: { name: '<명령어' }, status: 'online' })
 });
 
 client.on("guildMemberAdd", (member) => {
@@ -39,6 +40,36 @@ client.on('message', (message) => {
   if(message.content == '땅') {
     return message.reply('콩');
   }
+  if(message.content == '<si') {
+    let embed = new Discord.RichEmbed()
+    let img = 'https://cdn.discordapp.com/attachments/743671938861367319/753091363226255421/ddd.jpg';
+    var duration = moment.duration(client.uptime).format(" D [일], H [시간], m [분], s [초]");
+    embed.setColor('#186de6')
+    embed.setAuthor('server info of 스펀지밥', img)
+    embed.setFooter(`스펀지밥`)
+    embed.addBlankField()
+    embed.addField('RAM usage',    `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true);
+    embed.addField('running time', `${duration}`, true);
+    embed.addField('user',         `${client.users.size.toLocaleString()}`, true);
+    embed.addField('server',       `${client.guilds.size.toLocaleString()}`, true);
+    // embed.addField('channel',      `${client.channels.size.toLocaleString()}`, true);
+    embed.addField('Discord.js',   `v${Discord.version}`, true);
+    embed.addField('Node',         `${process.version}`, true);
+    
+    let arr = client.guilds.array();
+    let list = '';
+    list = `\`\`\`css\n`;
+    
+    for(let i=0;i<arr.length;i++) {
+      // list += `${arr[i].name} - ${arr[i].id}\n`
+      list += `${arr[i].name}\n`
+    }
+    list += `\`\`\`\n`
+    embed.addField('list:',        `${list}`);
+
+    embed.setTimestamp()
+    message.channel.send(embed);
+  }
 
   if(message.content == '<프로필') {
     let img = 'https://cdn.discordapp.com/attachments/751248598741811464/752855632788324372/ddd.jpg';
@@ -56,21 +87,25 @@ client.on('message', (message) => {
       .setTimestamp()
       .setFooter('동현이가 만듬', img)
 
+
     message.channel.send(embed)
-  } else if(message.content == 'help') {
-    let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
+  } else if(message.content == '<명령어') {
+    let helpImg = 'https://cdn.discordapp.com/attachments/743671938861367319/753091363226255421/ddd.jpg';
     let commandList = [
+      {name: '<명령어', desc: '도움말'},
       {name: 'ping', desc: '현재 핑 상태'},
-      {name: 'embed', desc: 'embed 예제1'},
-      {name: 'embed2', desc: 'embed 예제2 (help)'},
-      {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
-      {name: '!청소', desc: '텍스트 지움'},
+      {name: '<프로필', desc: '프로필 보기'},
+      {name: '<전체공지', desc: 'dm으로 전체 공지 보내기'},
+      {name: '<전체공지2', desc: 'dm으로 전체 embed 형식으로 공지 보내기'},
+      {name: '<청소', desc: '텍스트 지움'},
+      {name: '<초대코드', desc: '해당 채널의 초대 코드 표기'},
+      {name: '<초대코드2', desc: '봇이 들어가있는 모든 채널의 초대 코드 표기'},
     ];
     let commandStr = '';
     let embed = new Discord.RichEmbed()
-      .setAuthor('Help of 콜라곰 BOT', helpImg)
+      .setAuthor('Help of 스펀지밥', helpImg)
       .setColor('#186de6')
-      .setFooter(`콜라곰 BOT ❤️`)
+      .setFooter(`스펀지밥`)
       .setTimestamp()
     
     commandList.forEach(x => {
@@ -80,9 +115,53 @@ client.on('message', (message) => {
     embed.addField('Commands: ', commandStr);
 
     message.channel.send(embed)
-  }
-
-  if(message.content.startsWith('<전체공지')) {
+  } else if(message.content == '<초대코드2') {
+    client.guilds.array().forEach(x => {
+      x.channels.find(x => x.type == 'text').createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
+        .then(invite => {
+          message.channel.send(invite.url)
+        })
+        .catch((err) => {
+          if(err.code == 50013) {
+            message.channel.send('**'+x.channels.find(x => x.type == 'text').guild.name+'** 채널 권한이 없어 초대코드 발행 실패')
+          }
+        })
+    });
+  } else if(message.content == '<초대코드') {
+    if(message.channel.type == 'dm') {
+      return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
+    }
+    message.guild.channels.get(message.channel.id).createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
+      .then(invite => {
+        message.channel.send(invite.url)
+      })
+      .catch((err) => {
+        if(err.code == 50013) {
+          message.channel.send('**'+message.guild.channels.get(message.channel.id).guild.name+'** 채널 권한이 없어 초대코드 발행 실패')
+        }
+      })
+  } else if(message.content.startsWith('<전체공지2')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('<전체공지2'.length);
+      let embed = new Discord.RichEmbed()
+        .setAuthor('공지 of 스펀지밥')
+        .setColor('#186de6')
+        .setFooter(`스펀지밥`)
+        .setTimestamp()
+  
+      embed.addField('공지: ', contents);
+  
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(embed)
+      });
+  
+      return message.reply('공지를 전송했습니다.');
+    } else {
+      return message.reply('채널에서 실행해주세요.');
+    }
+  } else if(message.content.startsWith('<전체공지')) {
     if(checkPermission(message)) return
     if(message.member != null) { // 채널에서 공지 쓸 때
       let contents = message.content.slice('<전체공지'.length);
@@ -95,10 +174,12 @@ client.on('message', (message) => {
     } else {
       return message.reply('채널에서 실행해주세요.');
     }
-  }
-
-  if(message.content.startsWith('<청소')) {
-    if(checkPermission(message)) return
+  } else if(message.content.startsWith('<청소')) {
+    if(message.channel.type == 'dm') {
+      return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
+    }
+    
+    if(message.channel.type != 'dm' && checkPermission(message)) return
 
     var clearLine = message.content.slice('<청소 '.length);
     var isNum = !isNaN(clearLine)
@@ -112,10 +193,9 @@ client.on('message', (message) => {
 
         var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
         var count = parseInt(message.content.split(' ')[2])+1;
-        const _limit = 10;
         let _cnt = 0;
 
-        message.channel.fetchMessages({limit: _limit}).then(collected => {
+        message.channel.fetchMessages().then(collected => {
           collected.every(msg => {
             if(msg.author.id == user) {
               msg.delete();
