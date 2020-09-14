d20d480fe93dc5f04c3f75847a27cb70810f52c2
qp092488 <AsiaEurope0131@gmail.com>
2020-09-14 13:56:16 +0900

    add serverinfo

--- index.js
+++ index.js
@@ -169,14 +169,14 @@ client.on('message', (message) => {
     } else {
       return message.reply('채널에서 실행해주세요.');
     }
-  } else if(message.content.startsWith('>청소')) {
+  } else if(message.content.startsWith('<청소')) {
     if(message.channel.type == 'dm') {
       return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
     }
     
     if(message.channel.type != 'dm' && checkPermission(message)) return
 
-    var clearLine = message.content.slice('>청소 '.length);
+    var clearLine = message.content.slice('<청소 '.length);
     var isNum = !isNaN(clearLine)
 
     if(isNum && (clearLine <= 0 || 99 < clearLine)) {
